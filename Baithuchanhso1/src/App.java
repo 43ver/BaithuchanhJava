b79773d566632b0238dfc2d9737121afbfb77307
43ver <adcvinhtran@gmail.com>
2022-01-18 13:57:40 +0700

    *Create_Project_Baithuchanh18.1*

--- Baithuchanhso1/src/App.java
+++ Baithuchanhso1/src/App.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        double hsA, hsB , hsC, delta;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap he so a:");
         System.out.println("Hello, Java OOP!");
     }
 }
